62335dc086256009d686fd18058865806a1533b4
Aarez Ali Momin <aarez.umatiya111@gmail.com>
2024-09-04 13:44:47 -0500

    Changes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(){
+ return (
+ <>
+  
+  <form className="newttask">
+    <div className="checkk">
+      <label htmlFor="item">Enter New Task</label>
+      <input type="text" id="item" Type Here />
+    </div>
+    <button className="button"> Add </button>
+  </form>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+
+
+
+  <h1 className="header"> ToDoList </h1>
+  <ul className="list">
+    <li>
+      <label>
+        <input type="checkbox" ></input>
+        Enter Task 1
+      </label>
+      <button className="Delete1"> Delete Task</button>
+    </li>
+    <li>
+    <label>
+        <input type="checkbox" ></input>
+        Enter Task 2
+      </label>
+      <button className="Delete2">Delete Task</button>
+    </li>
+  </ul>
+ </>
+ )
 }
-
-export default App
