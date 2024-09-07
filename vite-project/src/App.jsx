49194e17e91d8b2c4beb041b6c5f73ae8db8dd28
Aarez Ali Momin <aarez.umatiya111@gmail.com>
2024-09-07 15:20:10 -0500

    updated changes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,37 +1,80 @@
+import { useEffect, useState } from 'react'
 import './styles.css'
 
 export default function App(){
+
+
+  const [newItem, setNewITem] = useState("")
+  const [todos, settodos] = useState(()=>{
+    const localvalue = localStorage.getItem("ITEMS")
+  
+    return JSON.parse(localvalue)
+  }
+
+  )
+  
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function submit(e){
+    e.preventDefault()
+    settodos(currenttodo => {
+      return [
+        ...todos, {id: crypto.randomUUID(), title: newItem, completed: false}, 
+      ]
+    })
+
+    setNewITem("")
+  }
+
+  function toggletodo(id, completed){
+    settodos(currenttodo => {
+      return currenttodo.map(todo => {
+        if(todo.id === id){
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deletetodo(id){
+    settodos(currenttodo =>{
+      return currenttodo.filter(todo => todo.id !== id)
+    })
+  }
+
  return (
  <>
   
-  <form className="newttask">
+  <form onSubmit={submit} className="newttask">
     <div className="checkk">
       <label htmlFor="item">Enter New Task</label>
-      <input type="text" id="item" Type Here />
+      <input value={newItem} 
+      onChange={e => setNewITem(e.target.value)}
+      type="text" id="item"/>
     </div>
     <button className="button"> Add </button>
   </form>
 
-
-
-
-
   <h1 className="header"> ToDoList </h1>
   <ul className="list">
-    <li>
+    {todos.length === 0 && "Add Tasks" }
+    {todos.map(todo => {
+      return (
+    <li key={todo.id}>
       <label>
-        <input type="checkbox" ></input>
-        Enter Task 1
+        <input type="checkbox" checked={todo.completed}
+        //onchange to check if item is selected or not
+        onChange={e => toggletodo(todo.id, e.target.checked)} ></input>
+        {todo.title}
       </label>
-      <button className="Delete1"> Delete Task</button>
+      <button onClick={() => deletetodo(todo.id)} className="Delete1"> Delete Task</button>
     </li>
-    <li>
-    <label>
-        <input type="checkbox" ></input>
-        Enter Task 2
-      </label>
-      <button className="Delete2">Delete Task</button>
-    </li>
+      )
+})}
   </ul>
  </>
  )
